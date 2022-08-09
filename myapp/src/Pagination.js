99c2c86cbc0cf4c03c7f5f0cee4587ff3fa5ab6e
TimZZZ <tiomsity@gmail.com>
2022-08-09 14:53:08 -0500

    prev and next btns working

--- myapp/src/Pagination.js
+++ myapp/src/Pagination.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import styles from "./Pagination.module.css";
+import React, { useState, useEffect } from 'react'
+import styles from './Pagination.module.css'
 
 const Pagination = ({
   todos,
@@ -10,47 +10,65 @@ const Pagination = ({
 }) => {
   const pageNumbers = [
     ...Array(Math.ceil(todos.length / pageCapacity + 1)).keys(),
-  ].slice(1);
+  ].slice(1)
 
   const pageNumberChangeHandler = (e) =>
-    setCurrentPageNumber(+e.target.innerText);
+    setCurrentPageNumber(+e.target.innerText)
 
   const pageNumberBtns = pageNumbers.map((number) => (
     <span
-      className={`${styles["page-number-btn"]} ${
-        number === currentPageNumber && styles["page-number-btn--active"]
+      className={`${styles['page-number-btn']} ${
+        number === currentPageNumber && styles['page-number-btn--active']
       }`}
       key={number}
       onClick={pageNumberChangeHandler}
     >
       {number}
     </span>
-  ));
+  ))
+
+  const paginationBtnClickHandler = (e) => {
+    const text = e.target.innerText
+
+    if (text === 'prev' && currentPageNumber > 1) {
+      setCurrentPageNumber((currPageNum) => currPageNum - 1)
+    }
+
+    if (text === 'next' && currentPageNumber < pageNumbers.at(-1)) {
+      setCurrentPageNumber((currPageNum) => currPageNum + 1)
+    }
+  }
 
   const capacityChangeHandler = (e) => {
-    setPageCapacity(e.target.value);
+    setPageCapacity(e.target.value)
 
     const newLastPage = [
       ...Array(Math.ceil(todos.length / e.target.value + 1)).keys(),
     ]
       .slice(1)
-      .at(-1);
+      .at(-1)
 
-    if (currentPageNumber > newLastPage) setCurrentPageNumber(newLastPage);
-  };
+    if (currentPageNumber > newLastPage) setCurrentPageNumber(newLastPage)
+  }
 
   return (
     <div className={styles.pagination}>
-      <button className={`${styles["pagination-btn"]} ${styles["prev-btn"]}`}>
+      <button
+        onClick={paginationBtnClickHandler}
+        className={`${styles['pagination-btn']} ${styles['prev-btn']}`}
+      >
         prev
       </button>
       {pageNumberBtns}
-      <button className={`${styles["pagination-btn"]} ${styles["next-btn"]}`}>
+      <button
+        onClick={paginationBtnClickHandler}
+        className={`${styles['pagination-btn']} ${styles['next-btn']}`}
+      >
         next
       </button>
       <select
         onChange={capacityChangeHandler}
-        className={styles["page-capacity-options"]}
+        className={styles['page-capacity-options']}
       >
         <option>10</option>
         <option>20</option>
@@ -58,7 +76,7 @@ const Pagination = ({
         <option>100</option>
       </select>
     </div>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
