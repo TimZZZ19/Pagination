df956fd2e6924395c7703ab083cbdb8a4d60cda8
TimZ <tiomsity@gmail.com>
2022-08-11 18:06:38 -0500

    rearranged the logic inside the useEffect hook in PageNumberBtns

--- myapp/src/components/pagination/PageNumberBtns.js
+++ myapp/src/components/pagination/PageNumberBtns.js
@@ -2,28 +2,25 @@ import React, { useState, useEffect } from "react";
 import styles from "./PageNumberBtns.module.css";
 
 const basePoint = 6;
-const defaultRange = { start: 0, end: 10 };
 
 function PageNumberBtns({
   pageNumbers,
   currentPageNumber,
   setCurrentPageNumber,
 }) {
-  const [range, setRange] = useState(defaultRange);
+  const [range, setRange] = useState({});
 
   useEffect(() => {
-    if (pageNumbers.at(-1) - basePoint <= 4) {
-      setRange(defaultRange);
-      return;
-    }
-
-    const relativeDifferenceFromBase = currentPageNumber - basePoint;
     let startIdx = 0;
 
-    if (relativeDifferenceFromBase > 0) startIdx = relativeDifferenceFromBase;
+    if (pageNumbers.at(-1) - basePoint > 4) {
+      const relativeDifferenceFromBase = currentPageNumber - basePoint;
 
-    if (relativeDifferenceFromBase + 10 > 20)
-      startIdx = pageNumbers.length - 10;
+      if (relativeDifferenceFromBase > 0) startIdx = relativeDifferenceFromBase;
+
+      if (relativeDifferenceFromBase + 10 > 20)
+        startIdx = pageNumbers.length - 10;
+    }
 
     setRange({
       start: startIdx,
