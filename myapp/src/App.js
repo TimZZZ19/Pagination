932b767cff65f89517e43b1e19ea4f1294012c98
TimZ <tiomsity@gmail.com>
2022-08-10 23:42:36 -0500

    finished

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,21 @@
-import axios from 'axios'
-import styles from './App.module.css'
-import { useState, useEffect } from 'react'
-import Pagination from './components/pagination/Pagination'
-import List from './components/list/List'
+import axios from "axios";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+import Pagination from "./components/pagination/Pagination";
+import List from "./components/list/List";
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const [pageCapacity, setPageCapacity] = useState(10)
-  const [currentPageNumber, setCurrentPageNumber] = useState(1)
+  const [todos, setTodos] = useState([]);
+  const [pageCapacity, setPageCapacity] = useState(10);
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
-  const url = `https://jsonplaceholder.typicode.com/todos`
+  const url = `https://jsonplaceholder.typicode.com/todos`;
 
   useEffect(() => {
     axios.get(url).then((res) => {
-      setTodos(res.data)
-    })
-  }, [url])
+      setTodos(res.data);
+    });
+  }, [url]);
 
   return (
     <div className={styles.App}>
@@ -32,7 +32,7 @@ function App() {
         todos={todos}
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- myapp/src/components/pagination/PageNumberBtns.js
+++ myapp/src/components/pagination/PageNumberBtns.js
@@ -1,20 +1,47 @@
-import React from 'react'
-import styles from './PageNumberBtns.module.css'
+import React, { useState, useEffect } from "react";
+import styles from "./PageNumberBtns.module.css";
+
+const basePoint = 6;
+const defaultRange = { start: 0, end: 10 };
 
 function PageNumberBtns({
   pageNumbers,
   currentPageNumber,
   setCurrentPageNumber,
 }) {
+  const [range, setRange] = useState(defaultRange);
+
+  useEffect(() => {
+    if (pageNumbers.at(-1) - basePoint <= 4) {
+      setRange(defaultRange);
+      return;
+    }
+
+    const relativeDifferenceFromBase = currentPageNumber - basePoint;
+    let startIdx = 0;
+
+    if (relativeDifferenceFromBase > 0) startIdx = relativeDifferenceFromBase;
+
+    if (relativeDifferenceFromBase + 10 > 20)
+      startIdx = pageNumbers.length - 10;
+
+    setRange({
+      start: startIdx,
+      end: startIdx + 10,
+    });
+  }, [currentPageNumber, pageNumbers]);
+
   const pageNumberChangeHandler = (e) =>
-    setCurrentPageNumber(+e.target.innerText)
+    setCurrentPageNumber(+e.target.innerText);
+
+  const currentRange = pageNumbers.slice(range.start, range.end);
 
   return (
     <>
-      {pageNumbers.map((number) => (
+      {currentRange.map((number) => (
         <span
-          className={`${styles['page-number-btn']} ${
-            number === currentPageNumber && styles['page-number-btn--active']
+          className={`${styles["page-number-btn"]} ${
+            number === currentPageNumber && styles["page-number-btn--active"]
           }`}
           key={number}
           onClick={pageNumberChangeHandler}
@@ -23,7 +50,7 @@ function PageNumberBtns({
         </span>
       ))}
     </>
-  )
+  );
 }
 
-export default PageNumberBtns
+export default PageNumberBtns;
